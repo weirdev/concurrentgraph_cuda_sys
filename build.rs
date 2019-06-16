6554172d37afc9ea0ddbfc10d92a9deef3908797
weirdev <weirdev@outlook.com>
2019-06-16 00:40:50 -0500

    Library path in build script must be absolute when this is linked from other crates.

--- build.rs
+++ build.rs
@@ -1,3 +1,3 @@
 fn main() {
-    println!("cargo:rustc-link-search=native=./lib/");
+    println!("cargo:rustc-link-search=native=/mnt/c/Users/Wesley/dev/concurrentgraph/concurrentgraph_cuda_sys/lib/");
 }
